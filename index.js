77694340e052c5242c5e5d1324bedcd8e63b5a0e
Soumabha Majumder <soumabhamajumder@gmail.com>
2024-10-16 18:34:33 +0530

    Update 1.0.1
    1. Added the feature to calculate the total sum of the Dice and display it to the user.
    2. Optimized the workflow of the code to make it smooth and efficient.

--- index.js
+++ index.js
@@ -2,30 +2,30 @@ function rollDice() {
 
   const noOfDice = document.getElementById("myInput").value;
   const diceResult = document.getElementById("diceResult");
+  const diceSum = document.getElementById("diceSum");
   const diceImages = document.getElementById("diceImages");
   const values = [];
   const images = [];
 
   let noOFDice2 = Number(noOfDice);
 
-  if((noOFDice2 >= 1) && (noOFDice2 <= 100)){
+  if ((noOFDice2 >= 1) && (noOFDice2 <= 100)) {
+    let total = 0;
     for (let i = 0; i < noOfDice; i++) {
       const value = Math.floor(Math.random() * 6) + 1;
       values.push(value);
-      images.push(`<img src="Dice_Images/Dice ${value}.png" alt="Dice ${value}">`)
+      images.push(`<img src="Dice_Images/Dice ${value}.png" alt="Dice ${value}">`);
+      total += value;
+      console.log(total);
     }
-  
-    if ((noOfDice > 100) || (noOfDice <= 0)) {
-      diceResult.textContent = `Please enter a valid number between 1 to 100 for rolling the dice`;
-    }
-    else {
-      diceResult.textContent = `Dice: ${values.join(', ')}`;
-      diceImages.innerHTML = images.join('');
-    }
-  
+
+    diceResult.textContent = `Dice: ${values.join(', ')}`;
+    diceSum.textContent = `Total Dice = ${total}`
+    diceImages.innerHTML = images.join('');
+
     document.getElementById("myInput").addEventListener("input", function () {
       let inputValue = this.value;
-  
+
       if (inputValue > 1000) {
         this.value = 1000;
       }
@@ -34,8 +34,7 @@ function rollDice() {
       }
     });
   }
-  else{
+  else {
     diceResult.textContent = `Please enter a valid number between 1 to 100 for rolling the dice`;
   }
-
 }
